518cf99a823d5e0e6f646e0acd27e95597f4d918
John Thompson <john@springframework.guru>
2023-11-07 13:03:31 -0500

    adding spring boot server module

---
+++ apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/ApifirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.apifirst.apifirstserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
