a2b91d8a9194e4d39c82a5560a010becde96674b
John Thompson <john@springframework.guru>
2023-11-17 13:47:25 -0500

    adding product get tests

--- apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/controllers/BaseTest.java
+++ apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/controllers/BaseTest.java
@@ -4,6 +4,7 @@ import guru.springframework.apifirst.apifirstserver.repositories.CustomerReposit
 import guru.springframework.apifirst.apifirstserver.repositories.OrderRepository;
 import guru.springframework.apifirst.apifirstserver.repositories.ProductRepository;
 import guru.springframework.apifirst.model.Customer;
+import guru.springframework.apifirst.model.Product;
 import jakarta.servlet.Filter;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,7 @@ public class BaseTest {
     public MockMvc mockMvc;
 
     Customer testCustomer;
+    Product testProduct;
 
     @BeforeEach
     void setUp() {
@@ -42,5 +44,6 @@ public class BaseTest {
                 .build();
 
         testCustomer = customerRepository.findAll().iterator().next();
+        testProduct = productRepository.findAll().iterator().next();
     }
 }
---
+++ apifirst-server/src/test/java/guru/springframework/apifirst/apifirstserver/controllers/ProductControllerTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.apifirst.apifirstserver.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class ProductControllerTest extends BaseTest {
+
+    @Test
+    void listProducts() throws Exception {
+        mockMvc.perform(get(ProductController.BASE_URL)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", greaterThan(0)));
+    }
+
+    @Test
+    void getProductById() throws Exception {
+        mockMvc.perform(get(ProductController.BASE_URL + "/{productId}", testProduct.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(testProduct.getId().toString())));
+    }
+}
