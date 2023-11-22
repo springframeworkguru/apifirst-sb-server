eac350711c3229dcae6b34004fadad3fd190fc1b
John Thompson <john@springframework.guru>
2023-11-22 11:28:20 -0500

    adding customer entity

---
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/domain/Customer.java
@@ -0,0 +1,29 @@
+package guru.springframework.apifirst.apifirstserver.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "char(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    private String email;
+    private String phone;
+}
