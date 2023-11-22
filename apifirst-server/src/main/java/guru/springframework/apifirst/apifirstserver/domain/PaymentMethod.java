71a5ebadab5912e342b2078472ad981436550eab
John Thompson <john@springframework.guru>
2023-11-22 13:06:39 -0500

    adding payment method

--- apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/domain/PaymentMethod.java
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/domain/PaymentMethod.java
@@ -6,7 +6,6 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -18,7 +17,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @Entity
-public class Customer {
+public class PaymentMethod {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -26,33 +25,14 @@ public class Customer {
     @Column(length = 36, columnDefinition = "char(36)", updatable = false, nullable = false)
     private UUID id;
 
-    @OneToOne
-    private Address shipToAddress;
+    @ManyToOne
+    private Customer customer;
 
-    @OneToOne
-    private Address billToAddress;
-
-    @Embedded
-    private Name name;
-
-    private String email;
-    private String phone;
-
-    @OneToMany(mappedBy = "customer")
-    private List<PaymentMethod> paymentMethods;
-
+    private String displayName;
+    private Integer cardNumber;
+    private Integer expiryMonth;
+    private Integer expiryYear;
+    private Integer cvv;
     private OffsetDateTime dateCreated;
     private OffsetDateTime dateUpdated;
 }
-
-
-
-
-
-
-
-
-
-
-
-
