dd24373e931afe3d31d3a429c487268d74ea35cb
John Thompson <john@springframework.guru>
2023-11-22 12:46:44 -0500

    adding address

--- apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/domain/Address.java
+++ apifirst-server/src/main/java/guru/springframework/apifirst/apifirstserver/domain/Address.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -16,23 +17,18 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @Entity
-public class Customer {
+public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "char(36)", updatable = false, nullable = false)
     private UUID id;
-
-    @OneToOne
-    private Address shipToAddress;
-
-    @OneToOne
-    private Address billToAddress;
-
-    @Embedded
-    private Name name;
-
-    private String email;
-    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zip;
+    private OffsetDateTime dateCreated;
+    private OffsetDateTime dateUpdated;
 }
